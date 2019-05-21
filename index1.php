0a4b3437ddd96437c82dc47742c7c9c0339a52cf
levantuan98 <tuan.lv164352@sis.hust.edu.vn>
2019-05-21 19:24:04 +0700

    21/5/19

--- index1.php
+++ index1.php
@@ -1,2 +1,27 @@
-<?php
-echo "Automatic deploy";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div>
+            <h1>Calculation BMI(Body Mass Index)</h1>
+        </div>
+        <div>
+            <form>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Height</label>
+                    <input type="number" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter height">
+                 </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Weight</label>
+                    <input type="number" class="form-control" id="exampleInputPassword1" placeholder="Enter weight">
+                </div>
+                <button type="submit" class="btn btn-primary">Calculate</button>
+            </form>
+        </div>
+</body>
+</html>
